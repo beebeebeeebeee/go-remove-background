f73f8eefeed1af0e3129938d3d640b8a7fcba19d
Fung Mak <fung.mak@keysocapp.com>
2024-11-04 11:49:04 +0800

    fixs

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -3,6 +3,6 @@
 printenv | tee .env >/dev/null
 
 BASE_URL=$(grep BASE_URL .env | cut -d '=' -f2)
-sed -i '' "s|%BASE_URL%|$BASE_URL|g" static/index.html
+sed -i "s|%BASE_URL%|$BASE_URL|g" static/index.html
 
 ./main
